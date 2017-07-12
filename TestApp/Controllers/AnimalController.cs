6077f0b485bff08dde729a804238498d5315c8fc
Micah Netz <mnetz@2ndwatch.com>
2017-07-12 12:21:09 -0700

    Starting repo

---
+++ TestApp/Controllers/AnimalController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Zoo()
+        {
+            Animal test = new Animal();
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestApp/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class Animal
+    {
+        public string Species { get; set; }
+        public int Legs { get; set; }
+        public IEnumerable<string> Diet { get; set; }
+        public Animal() { Species = "Puppy"; Legs = 4; Diet =  new List<string>{ "Dog food", "Newspapers"}; }
+    }
+}
